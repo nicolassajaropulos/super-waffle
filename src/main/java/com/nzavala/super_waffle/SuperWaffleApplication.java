f67d97f51013df89ffc249299e763558e4631276
Kairos Nicolas Zavala Sajaropulos <nem.nzavala@coppel.com>
2024-08-13 10:39:39 -0700

    Initial commit

---
+++ src/main/java/com/nzavala/super_waffle/SuperWaffleApplication.java
@@ -0,0 +1,13 @@
+package com.nzavala.super_waffle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperWaffleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperWaffleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nzavala/super_waffle/SuperWaffleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nzavala.super_waffle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperWaffleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
